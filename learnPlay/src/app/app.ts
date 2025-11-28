65c5a66850e21d7bf09df610fd9714706b7464e4
Maurice <ombewamaurice79@gmail.com>
2025-11-28 14:29:48 +0300

    Add landing page component and module structure

--- learnPlay/src/app/app.ts
+++ learnPlay/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LandingPage } from './landing-page/landing-page';
+// import { LandingPage } from './landing-page-module/components/landing-page/landing-page';
+import { LandingPageComponent } from './landing-page-module/components/landing-page-component/landing-page-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LandingPage],
+  imports: [RouterOutlet, LandingPageComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
